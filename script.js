b76b147e14ee8ac243677c4f83dac09afe601e22
Mukhtar Lawal Abdullahi <mukhtarlawalabdullahi96@gmail.com>
2026-06-30 09:01:58 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector(".menu-btn");
-const navLinks = document.querySelector(".nav-links");
-
-menuBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
-});
